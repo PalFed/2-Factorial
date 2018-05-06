4ee8807d8841b711944182baaca0b67938f1a780
Ben <bholness@sektr.com>
2018-05-06 18:58:58 -0500

    Add message about installing the web extension

--- demoSite/secret.php
+++ demoSite/secret.php
@@ -19,19 +19,19 @@ if (isset($_POST['secret']))
 <body>
 <h1>2 Factorial Demo : Setup 2FA</h1>
 <?php
-if ($authorised) print "<p><strong>You are currently authorized</strong></p>";
-else print "<p><strong>You are not currently authorized</strong></p>";
+if ($authorised) print "<p><strong>You are currently authorized</strong> <a href='index.php'>Home</a></p>";
+else print "<p><strong>You are not currently authorized</strong> <a href='index.php'>Home</a></p>";
 ?>
 <p>Please enter your 2FA secret. This must match what has been entered in the 2 Factorial preferences as the secret for <?php echo $_SERVER['HTTP_HOST']; ?>. <?php
 
 if (isset($_SESSION['ServerSideSecret'])) print "The current secret is not shown.";
 else print "No secret has been set.";
-    ?> <a href='index.php'>Home</a></p>
+    ?></p>
 <p>Note: The secret is stored in the session, which will timeout after <?php echo number_format(ini_get("session.gc_maxlifetime"), 0); ?> seconds of inactivity.</p>
 <form action="secret.php" method="post">
     Secret: <input title="Secret" type="password" name="secret"> <input type='submit' title="Save Secret" value='Save Secret'>
 </form>
 <hr>
 <h2>Real Installation</h2>
-<p>In a real installation, the user would go to their preferences page, switch on 2FA via 2Factorial and enter their secret there to be saved on the server. They would then enter the same secret in the 2Factorial Extension Preferences page.</p>
+<p>In a real installation, the user would go to their preferences page, switch on 2FA via 2 Factorial and enter their secret there to be saved on the server. They would then enter the same secret in the 2 Factorial Extension Preferences page.</p>
 </body></html>
